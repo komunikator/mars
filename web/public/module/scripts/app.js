b219b6e458d7e0d54e087efd693bbdd9bdffebc8
aeadigt <aea@digt.ru>
2015-11-20 15:30:52 +0300

    Добавлено указание высоты

--- web/public/module/scripts/app.js
+++ web/public/module/scripts/app.js
@@ -2,13 +2,26 @@ Ext.Loader.setConfig({enabled: true});
 Ext.Loader.setPath('IVR', '/javascripts/app');
 Ext.onReady(function() {
     Ext.tip.QuickTipManager.init();
-    Ext.create('Ext.container.Viewport', {
+
+    var extScript = Ext.create('IVR.view.scripts.Editor', {
+                position: 'absolute !important',
+    });
+
+    var ext1Script = Ext.create('Ext.Panel', {
+                title: lang.user + ': <b>' + window['_username'] + '</b>',
+    });
+
+    var scripts = Ext.create('Ext.container.Viewport', {
         renderTo: Ext.getBody(),
         layout: 'fit',
         items:
-	        Ext.create('Ext.Panel', {
-			    title: lang.user + ': <b>' + window['_username'] + '</b>',
-			    items: Ext.create('IVR.view.scripts.Editor', {})
-			})
-   })
+        [
+            ext1Script,
+            extScript
+        ]
+    });
+
+    //extScript.setHeight(200);
+    //ext1Script.setHeight(200);
+    scripts.setHeight(200);
 });
