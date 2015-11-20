d048843e648c6b2fe6ae49a9e8c672972b88d8c3
aeadigt <aea@digt.ru>
2015-11-20 16:20:45 +0300

    Правки в scripts app

--- web/public/module/scripts/app.js
+++ web/public/module/scripts/app.js
@@ -1,27 +1,22 @@
 Ext.Loader.setConfig({enabled: true});
 Ext.Loader.setPath('IVR', '/javascripts/app');
+
 Ext.onReady(function() {
     Ext.tip.QuickTipManager.init();
 
-    var extScript = Ext.create('IVR.view.scripts.Editor', {
-                position: 'absolute !important',
-    });
-
-    var ext1Script = Ext.create('Ext.Panel', {
-                title: lang.user + ': <b>' + window['_username'] + '</b>',
-    });
-
-    var scripts = Ext.create('Ext.container.Viewport', {
+    Ext.create('Ext.container.Viewport', {
         renderTo: Ext.getBody(),
-        layout: 'fit',
+        layout: 'border',
         items:
         [
-            ext1Script,
-            extScript
+            Ext.create('Ext.Panel', {
+                height: 64,
+                region: 'north',
+                title: lang.user + ': <b>' + window['_username'] + '</b><p>Выход</p>'
+            }),
+            Ext.create('IVR.view.scripts.Editor', {
+                region: 'center'
+            })
         ]
     });
-
-    //extScript.setHeight(200);
-    //ext1Script.setHeight(200);
-    scripts.setHeight(200);
 });
