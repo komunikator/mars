1fa714f702462725557cc737759a9aaebc301043
aeadigt <aea@digt.ru>
2015-11-23 12:53:00 +0300

    Добавлен лого марс к компоненту header

--- web/public/javascripts/app/view/Header.js
+++ web/public/javascripts/app/view/Header.js
@@ -4,5 +4,5 @@ Ext.define('IVR.view.Header', {
     title: lang.mainTitle,
     height: 64,
     region: 'north',
-    title: lang.user + ': <b>' + window['_username'] + '</b><p>Выход</p>'
+    title: '<div style="float: left; background:url(../../apple-touch-icon-152x152.png) no-repeat center center; height: 54px; width: 152px"></div><div style="float: right">' + lang.user + ': <b>' + window['_username'] + '</b><p><a href="/logOut">Выход</a></p></div>'
 });
