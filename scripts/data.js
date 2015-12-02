ab55c603c33ff8cb3a9e08d37e3ca82397e732a9
aeadigt <aea@digt.ru>
2015-12-02 16:28:21 +0300

    Добавлен редактор скриптов

---
+++ scripts/data.js
@@ -0,0 +1,5 @@
+s = {
+    str:  /**/ 'namevoice.wav'   /* {"description": "1", "typeField": "string"}  */,
+    num:  /**/  111              /* {"description": "2", "typeField": "number"}  */,
+    bool: /**/  false            /* {"description": "3", "typeField": "boolean"}  */
+}
