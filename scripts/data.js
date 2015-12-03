8fd95ec313abbe2e159bd70c68a7c2fc15c69ccb
aeadigt <aea@digt.ru>
2015-12-03 09:09:07 +0300

    Внесены мелкие правки

--- scripts/data.js
+++ scripts/data.js
@@ -1,4 +1,4 @@
-s = {
+exports.src = {
     str:  /**/ 'namevoice.wav'   /* {"description": "1", "typeField": "string"}  */,
     num:  /**/  111              /* {"description": "2", "typeField": "number"}  */,
     bool: /**/  false            /* {"description": "3", "typeField": "boolean"}  */
