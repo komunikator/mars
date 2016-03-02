f6df950c12b615c755a0178214c4ed137c5a1aa9
aeadigt <aea@digt.ru>
2016-03-02 16:25:39 +0300

    Возвращены изменения

---
+++ scripts/08 Циклы.js
@@ -0,0 +1,28 @@
+﻿exports.src = {
+    action: {func: function (self) {
+            self.session.user.stepID = 0;
+            self.session.user.step = [
+                {text: 'Привет, это шаг №1'},
+                {text: 'Еще раз привет, это шаг №2'},
+                {text: 'Снова привет, это шаг №3', action: {hangUp: true}}
+            ];
+            self.cb();
+        },
+        next: {
+            ttsPlay: {text: 'Инициализация',
+                next: {
+                    mark: 'main',
+                    ttsPlay: {text: function (self) {
+                            return self.session.user.step[self.session.user.stepID].text;
+                        },
+                        next: function (self) {
+                            var user = self.session.user,
+                                    next = user.step[user.stepID] && user.step[user.stepID].action || {goto: 'main'};
+                            user.stepID++;
+                            return next
+                        }}
+                }
+            }
+        }
+    }
+};
