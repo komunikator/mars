ca3228471f8f8d5892349ae8794756bebe91c373
dda <dda@digt.ru>
2016-03-17 14:08:27 +0300

    обновлён файл

--- modules/medTest.js
+++ modules/medTest.js
@@ -4,24 +4,36 @@ var config = bus.config;
 bus.onRequest('getSpecialties', function (param, cb) {
     param.query = param.query || {};
     var data = require('../scripts/08 Запись к врачу/список специалистов.js');
-    // data = data.join(',');
-    // console.log('----------------------');
-    // console.log(data);
-    // console.log('----------------------');
-
     cb(null, data);
 });
 
 bus.onRequest('getDoctors', function (param, cb) {
-    param = param.specialties || {};
-    console.log('=====================param');
-    console.log(param);
-    console.log('param=======================');
+    // param = param.specialties || {};
     var data = require('../scripts/08 Запись к врачу/список врачей.js');
-    // data = data.join(',');
-    // console.log('----------------------');
-    // console.log(data);
-    // console.log('----------------------');
+    cb(null, data);
+});
 
+bus.onRequest('getDates', function (param, cb) {
+    var data = require('../scripts/08 Запись к врачу/даты приёма.js');
+    cb(null, data);
+});
+
+bus.onRequest('getTimes', function (param, cb) {
+    var data = require('../scripts/08 Запись к врачу/время приёма.js');
+    cb(null, data);
+});
+
+bus.onRequest('getYear', function (param, cb) {
+    var data = require('../scripts/08 Запись к врачу/годы рождения.js');
+    cb(null, data);
+});
+
+bus.onRequest('getDayMonth', function (param, cb) {
+    var data = require('../scripts/08 Запись к врачу/число и месяц рождения.js');
+    cb(null, data);
+});
+
+bus.onRequest('getFIO', function (param, cb) {
+    var data = require('../scripts/08 Запись к врачу/ФИО пациентов.js');
     cb(null, data);
 });
