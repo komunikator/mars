0259aca23a9f76da43755ef516382a5ef7d0ab1a
dda <dda@digt.ru>
2016-03-23 12:44:38 +0300

    удалён сценарий

--- scripts/08 Запись к врачу/doctors_1.js
+++
@@ -1,7 +0,0 @@
-exports.src = function(self){
-    var user = self.session.user;
-    user.requestRes = self.requestRes || ''; // запоминаем список врачей
-    var txt = user.txt;
-    user.txt = '';
-    return txt;
-}
