656656202781a937257d76d6642b29174c24d990
ale <alexander.elkin@digt.ru>
2016-04-22 18:22:58 +0300

    ivona refresh keys bugfix

--- modules/speech/speech_ivona.js
+++ modules/speech/speech_ivona.js
@@ -1,11 +1,13 @@
 var bus = require('../../lib/system/bus'),
         config = bus.config;
 
-var ivona_speech = new (require('ivona-node'))(config.get("ivona_speech") || {});
+var ivona_speech;
 
 bus.on('ttsLaunch', function (data) {
     data.type = data.type || config.get("def_tts");
     if (data.type === 'ivona') {
+        if (!ivona_speech)
+            ivona_speech = new (require('ivona-node'))(config.get("ivona_speech") || {});
         var fileWriter = require('fs').createWriteStream(data.file);
         fileWriter.on('finish', data.cb);
         ivona_speech.createVoice(data.text, {
