a52a8be5f862fdb3b439060630df4dd5e7071931
aeadigt <aea@digt.ru>
2016-05-12 15:58:09 +0300

    Добавлена категория rotation

--- config/config.js
+++ config/config.js
@@ -63,7 +63,8 @@
                 "call",
                 "task",
                 "error",
-                "http"
+                "http",
+                "rotation"
             ]
         },
         {
@@ -131,10 +132,10 @@
         },
         {
             "type": "file",
-            "filename": "logs/softphone.log",
+            "filename": "logs/rotation.log",
             "maxLogSize": 1048576,
             "backups": 10,
-            "category": "softphone"
+            "category": "rotation"
         }
     ]
 }
