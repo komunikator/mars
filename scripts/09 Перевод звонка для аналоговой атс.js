3ad1f0d393e8d10a4c5a8bccf3e3c8305d0338cf
aeadigt <aea@digt.ru>
2016-05-27 11:21:54 +0300

    Добавлена возможность указывать несколько дтмф сигналов

--- scripts/09 Перевод звонка для аналоговой атс.js
+++ scripts/09 Перевод звонка для аналоговой атс.js
@@ -4,62 +4,20 @@ exports.src = {
 		file: 'media/Добро_пожаловать_в демонстрацию_системы_MARS.wav',
 		next: {
 
-			// Посылка hf
 			sendDtmf: {
-				text: 'hf',
+				text: 'R100',
 				next: {
 
 					// Ждем 1 с
 					wait: {
 						time: 1,
+
 						next: {
-
-							// Посылка номер абонента
-							sendDtmf: {
-								text: '1',
-								next: {
-
-									// Ждем 1 с
-									wait: {
-										time: 1,
-										next: {
-
-											sendDtmf: {
-												text: '0',
-												next: {
-
-													// Ждем 1 с
-													wait: {
-														time: 1,
-														next: {
-
-															sendDtmf: {
-																text: '0',
-																next: {
-
-																	// Ждем 1 с
-																	wait: {
-																		time: 1,
-
-																		next: {
-																			hangUp: true
-																		}
-																	}
-																}
-															}
-														}
-													}
-												}
-											}
-										}
-									}
-								}
-							}
+							hangUp: true
 						}
 					}
 				}
 			}
-
 		}
 	}
 }
