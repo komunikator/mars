25835eb29779d4f9ac2119b025e4a8c39851c1ca
zazeblik <zazeblik@mail.ru>
2016-06-09 12:18:26 +0300

    Update sipProxy.js

--- modules/sipProxy.js
+++ modules/sipProxy.js
@@ -3,9 +3,9 @@ var bus = require('../lib/system/bus');
 function init() {
     var worker = require('child_process').fork(__dirname + '/sipProxy/index', {silent: true, execPath: 'node'});
 
-    // worker.on('error', function (err) {
-    //     bus.emit('message', {type: 'error', msg: err});
-    // });
+    worker.on('error', function (err) {
+        bus.emit('message', {type: 'error', msg: err});
+    });
 
     bus.setWorker(worker);
 }
