5a20f0ffbfa71c409ba2fb2a79e1f942b88c765b
zazeblik <zazeblik@mail.ru>
2016-06-30 17:15:40 +0300

    group Call

--- modules/sipProxy/index.js
+++ modules/sipProxy/index.js
@@ -9,120 +9,193 @@ var contacts = {};
 var realm = os.hostname();
 var lastToSend;
 var registry = {};
-var timer;
+
 process.on('uncaughtException', function (e) {
     bus.emit('message', {category: 'debug', type: 'error', msg: e.toString()});
-    //console.log(e);
 });
 
 function sendContacts() {
-  var toSendContacts = [];
-  for(var item in contacts) {
-    if (!contacts[item][0].params || (contacts[item][0].params.expires != 0)){
-      toSendContacts.push(item + '@' + sip.parseUri(contacts[item][0].uri).host + ':' + sip.parseUri(contacts[item][0].uri).port);
-    } 
-  };
-  if (lastToSend != toSendContacts){
-    bus.emit('setSipClients', JSON.stringify(toSendContacts));
-  }
-  lastToSend = toSendContacts;
+    var toSendContacts = [];
+    for (var item in contacts) {
+        if (!contacts[item][0].params || (contacts[item][0].params.expires != 0)) {
+            toSendContacts.push(item + '@' + sip.parseUri(contacts[item][0].uri).host + ':' + sip.parseUri(contacts[item][0].uri).port);
+        }
+    }
+    ;
+    if (lastToSend != toSendContacts) {
+        bus.emit('setSipClients', JSON.stringify(toSendContacts));
+    }
+    lastToSend = toSendContacts;
+}
+
+function setDefaultDomain(uri) {
+    var tmp_uri = sip.parseUri(uri);
+    tmp_uri.host = require("ip").address();
+    tmp_uri.port = '5060';
+    return sip.stringifyUri(tmp_uri);
+}
+
+function setDefaultUri(user, uri) {
+    uri = setDefaultDomain(uri);
+    var tmp_uri = sip.parseUri(uri);
+    tmp_uri.user = user;
+    return sip.stringifyUri(tmp_uri);
 }
 
 function sipDigestRegister(rq, username) {
-  var userinfo = registry[username];
-  if(!userinfo) { // we don't know this user and answer with a challenge to hide this fact 
-    var session = {realm: realm};
-    sip.send(digest.challenge({realm: realm}, sip.makeResponse(rq, 401, 'Authentication Required')));
-  } else {
-    userinfo.session = userinfo.session || {realm: realm};
-    registry[username] = userinfo;
-    if(!digest.authenticateRequest(userinfo.session, rq, {user: username, password: userinfo.password})) {
-      sip.send(digest.challenge(userinfo.session, sip.makeResponse(rq, 401, 'Authentication Required')));
+    var userinfo = registry[username];
+    if (!userinfo) {
+        var session = {realm: realm};
+        sip.send(digest.challenge({realm: realm}, sip.makeResponse(rq, 401, 'Authentication Required')));
+    } else {
+        userinfo.session = userinfo.session || {realm: realm};
+        registry[username] = userinfo;
+
+        if (!digest.authenticateRequest(userinfo.session, rq, {user: username, password: userinfo.password})) {
+            sip.send(digest.challenge(userinfo.session, sip.makeResponse(rq, 401, 'Authentication Required')));
+        }
+        else {
+            if (contacts[username] && contacts[username].timer) {
+                clearTimeout(contacts[username].timer);
+            }
+
+            userinfo.contact = rq.headers.contact;
+            registry[username] = userinfo;
+            var rs = sip.makeResponse(rq, 200, 'OK');
+            contacts[username] = rq.headers.contact;
+            contacts[username].timer = setTimeout(function () {
+                delete contacts[username];
+                sendContacts();
+            }, parseInt(rq.headers.expires || (rq.headers.contact && rq.headers.contact[0] && rq.headers.contact[0].params.expires)) * 1000);
+            sendContacts();
+            rs.headers.contact = rq.headers.contact;
+            sip.send(rs);
+        }
     }
-    else {
-      clearTimeout(timer);
-      timer = setTimeout(function () {
-        delete contacts[username]; 
-        sendContacts();
-      },parseInt(rq.headers.expires || (rq.headers.contact && rq.headers.contact[0] && rq.headers.contact[0].params.expires)) * 1000);
-      userinfo.contact = rq.headers.contact;
-      registry[username] = userinfo;
-      var rs = sip.makeResponse(rq, 200, 'Ok');
-      
-      contacts[username] = rq.headers.contact;
-      sendContacts();
-      rs.headers.contact = rq.headers.contact;
-      sip.send(rs);
-    }
-  }
 }
-if (conf){
-    for (var i = 0; i < conf.length; i++){
-      registry[conf[i].user] = { password: conf[i].password}
+
+if (conf) {
+    for (var i = 0; i < conf.length; i++) {
+        registry[conf[i].user] = {password: conf[i].password};
+        if (conf[i].group) {
+            registry[conf[i].user].group = conf[i].group;
+        }
     }
 }
 
 bus.emit('message', {msg: 'sip_proxy started:' + require("ip").address()});
 
 proxy.start({
-  logger: {
-    recv: function (m, i) {
-      bus.emit('message', {category: 'sip_proxy', msg: 'RECV from ' + i.protocol + ' ' + i.address + ':' + i.port + '\n' + sip.stringify(m) + '\n'});
-    },
-    send: function (m, i) {
-      bus.emit('message', {category: 'sip_proxy', msg: 'SEND to ' + i.protocol + ' ' + i.address + ':' + i.port + '\n' + sip.stringify(m) + '\n'});
-    },
-    error: function (e) {
-      bus.emit('message', {category: 'error', type: 'error', msg: e.stack});
+    logger: {
+        recv: function (m, i) {
+            bus.emit('message', {category: 'sip_proxy', msg: 'RECV from ' + i.protocol + ' ' + i.address + ':' + i.port + '\n' + sip.stringify(m) + '\n'});
+        },
+        send: function (m, i) {
+            bus.emit('message', {category: 'sip_proxy', msg: 'RECV from ' + i.protocol + ' ' + i.address + ':' + i.port + '\n' + sip.stringify(m) + '\n'});
+        },
+        error: function (e) {
+            bus.emit('message', {category: 'error', type: 'error', msg: e.stack});
+        }
     }
-  }
-}, function(rq) {
-  if(rq.method === 'REGISTER') {  
-    var username = sip.parseUri(rq.headers.to.uri).user;
-    sipDigestRegister(rq, username);
-  }
-  else {
-    if (rq.headers.contact && rq.headers.contact[0].uri){
-      var tmp_uri = sip.parseUri(rq.headers.contact[0].uri);
-      tmp_uri.host = require("ip").address();
-      tmp_uri.port = '5060';
-      rq.headers.contact[0].uri = sip.stringifyUri(tmp_uri);
+}, function (rq) {
+    if (rq.method === 'REGISTER') {
+        var username = sip.parseUri(rq.headers.to.uri).user;
+        sipDigestRegister(rq, username);
+    } else {
+        if (rq.headers.contact && rq.headers.contact[0].uri) {
+            rq.headers.contact[0].uri = setDefaultDomain(rq.headers.contact[0].uri);
+        }
+        var user = sip.parseUri(rq.uri).user;
+        if (contacts[user] && Array.isArray(contacts[user]) && contacts[user].length > 0) {
+            rq.uri = contacts[user][0].uri;
+            proxy.send(rq);
+        } else {
+            if (rq.method === 'INVITE') {
+                var group = [];
+                var invites = [];
+                var timer = setTimeout(function () {
+                    proxy.send(sip.makeResponse(rq, 486, 'Busy Here'));
+                    cancelInvite({headers: {to: {uri: ''}}});
+                }, 10 * 1000);
+                function cancelInvite(rs) {
+                    var inv_rq = invites.shift();
+                    if (!inv_rq)
+                        return;
+                    if (inv_rq.headers.to.uri == rs.headers.to.uri)
+                        return cancelInvite();
+                    var cnc_rq = sip.copyMessage(inv_rq);
+                    cnc_rq.method = 'CANCEL';
+                    cnc_rq.headers.cseq.method = 'CANCEL';
+                    delete cnc_rq.content;
+                    proxy.send(cnc_rq, function () {
+                        cancelInvite(rs);
+                    });
+                }
+                ;
+                for (var item in registry)
+                    if (user == registry[item].group && contacts[item]) {
+                        group.push(item);
+                    }
+                if (group.length) {
+                    toDo();
+                    function toDo() {
+                        var item = group.shift();
+                        if (!item)
+                            return;
+                        var inv_rq = sip.copyMessage(rq);
+                        inv_rq.uri = contacts[item][0].uri;
+                        inv_rq.headers.to.uri = setDefaultUri(item, rq.headers.to.uri);
+                        invites.push(inv_rq);
+                        proxy.send(inv_rq, function (rs) {
+                            inv_rq.headers.to = rs.headers.to;
+                            if (rs.status != 487) {
+                                rs.headers.via.shift();
+                                proxy.send(rs);
+                            }
+                            if (rs.status == 200 && rs.headers.cseq.method == 'INVITE') {
+                                clearTimeout(timer);
+                                cancelInvite(rs);
+                            }
+                            toDo()
+                        });
+                    }
+                } else
+                    proxy.send(sip.makeResponse(rq, 404, 'Not Found'));
+            } else
+                proxy.send(sip.makeResponse(rq, 404, 'Not Found'));
+        }
     }
-    var user = sip.parseUri(rq.uri).user;
-    if(contacts[user] && Array.isArray(contacts[user]) && contacts[user].length > 0) {
-      rq.uri = contacts[user][0].uri;
-      proxy.send(sip.makeResponse(rq, 100, 'Trying'));
-      proxy.send(rq);
-    }
-    else {
-      proxy.send(sip.makeResponse(rq, 404, 'Not Found'));
-    }
-  }
 });
 
 bus.on('refresh', function (type) {
-  if (type == 'configData') {
-    bus.request('sipClients', {}, function (err, data) {
-      if (data){
-        var tmp=[];
-        for (var i = 0; i < data.length; i++){
-          tmp[i] = data[i].user;
-        }
-  
-        for (var i = 0; i < data.length; i++){
-          if (!registry[data[i].user]){
-            registry[data[i].user] = { password: data[i].password}
-          } else {
-            for( var item in registry ) {
-              if (tmp.indexOf(item)+1 == 0){
-                delete registry[item];
-              }
-            } 
-          }
-        } 
-      }
-    });
-    sendContacts();
-  }
-});
+    if (type == 'configData') {
+        bus.request('sipClients', {}, function (err, data) {
+            if (data) {
+                var tmp = [];
+                for (var i = 0; i < data.length; i++) {
+                    tmp[i] = data[i].user;
+                }
 
+                for (var i = 0; i < data.length; i++) {
+                    if (data[i].group && tmp.indexOf(data[i].group) == -1) {
+                        tmp.push(data[i].group);
+                    }
+                }
+
+                for (var i = 0; i < data.length; i++) {
+                    if (!registry[data[i].user]) {
+
+                        registry[data[i].user] = {password: data[i].password}
+                    } else {
+                        for (var item in registry) {
+                            if (tmp.indexOf(item) + 1 == 0) {
+                                delete registry[item];
+                            }
+                        }
+                    }
+                }
+            }
+        });
+        sendContacts();
+    }
+});
