e03cac92c4f01873276bf86ecf15b8b33426741e
zazeblik <zazeblik@mail.ru>
2017-03-06 16:42:13 +0300

    iframe view menu

--- www/root/main/javascripts/app/model/Menu.js
+++ www/root/main/javascripts/app/model/Menu.js
@@ -1,17 +1,33 @@
 Ext.define('IVR.model.Menu', {
     extend: 'Ext.data.Model',
-    fields: ['id','text', 'cls','expanded'],
+    fields: ['id', 'text', 'cls', 'expanded'],
     fields2: [
         { name: 'id', type: 'int', mapping: 'Id' },
         { name: 'text', type: 'string', mapping: 'Text' },
         { name: 'leaf', type: 'boolean', mapping: 'Leaf' },
         { name: 'loaded', type: 'boolean', mapping: 'Loaded', defaultValue: false },
-        { name: 'Properties'},
+        { name: 'Properties' },
         { name: 'expanded', defaultValue: true }
     ],
     autoLoad: true,
     proxy: {
-	type: 'ajax',
-	url: _webPath + '/menu.json'
+        type: 'ajax',
+        url: getMenuPath()
     }
 });
+
+function inIframe() {
+    try {
+        return window.self !== window.top;
+    } catch (e) {
+        return true;
+    }
+}
+
+function getMenuPath() {
+    if (inIframe()) {
+        return _webPath + '/frame_menu.json'
+    } else {
+        return _webPath + '/menu.json';
+    }
+}
