e1afb75501de3a91f531b7492b1d3faa87abf48e
aea <aea@digt.ru>
2018-02-12 13:38:03 +0300

    Added ability to make requests.

--- scripts/25 B24 Chatbot.js
+++ scripts/25 B24 Chatbot.js
@@ -1,10 +1,27 @@
-exports.src = async function (self, cb) {
-    var promise = new Promise((resolve) => {
-        setTimeout( () => {
-            return resolve('Ответ на сообщение: ', self.message);
-        }, 1000);
-    });
+// Для генерации ответа на сообщения вызвать cb(self);
+// Преждевременно присвоить сформированный ответ в self.answer
 
-    self.answer = await promise;
-    cb(self);
+exports.src = async function (self, cb) {
+    switch(self.message) {
+        case 'что горит':
+            let params = {};
+
+            self.request(self, params, (err, data) => {
+                if (err) {
+                    self.answer = 'Ошибка при получении тасков';
+                    break;
+                }
+                self.answer = data;
+                cb(self);
+            });
+            break;
+        default:
+            self.answer = await new Promise((resolve) => {
+                setTimeout( () => {
+                    return resolve(`Стандартный ответ на сообщение: ${self.message}`);
+                }, 1000);
+            });
+            cb(self);
+            break;
+    }
 }
