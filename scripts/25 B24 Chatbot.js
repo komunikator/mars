b6efc43545eecd5f4dd920d2dafc07afd22cd940
aea <aea@digt.ru>
2018-02-13 10:17:50 +0300

    Added debug information.

--- scripts/25 B24 Chatbot.js
+++ scripts/25 B24 Chatbot.js
@@ -4,18 +4,22 @@
 exports.src = async function (self, cb) {
     switch(self.message) {
         case 'что горит':
+            console.log('24 B24 Chatbot start request что горит');
             let params = {};
 
             self.request(self, params, (err, data) => {
+                console.log('24 B24 Chatbot callback');
                 if (err) {
+                    console.log(err);
                     self.answer = 'Ошибка при получении тасков';
-                    break;
+                } else {
+                    self.answer = data;
                 }
-                self.answer = data;
                 cb(self);
             });
             break;
         default:
+            console.log('24 B24 Chatbot start default');
             self.answer = await new Promise((resolve) => {
                 setTimeout( () => {
                     return resolve(`Стандартный ответ на сообщение: ${self.message}`);
