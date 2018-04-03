391414ebe546b2e38562c1de956d0f2a7d8bc5b5
Debian <admin@ip-172-31-19-243.eu-central-1.compute.internal>
2018-04-03 07:36:48 +0000

    Add change script chatbot

--- scripts/25 B24 Chatbot.js
+++ scripts/25 B24 Chatbot.js
@@ -35,6 +35,191 @@ exports.src = async function (self, cb) {
         });
     }
 
+    async function getBotList() {
+        return new Promise((resolve, reject) => {
+            let request = {
+                requestTimeout: 35000,
+                url: self.url,
+                method: 'imbot.chat.user.list',
+                settings: {
+                    access_token: self.body['auth']['access_token']
+                }
+            };
+
+            self.request(request, (err, data) => {
+                if (err) return reject(err);
+                resolve(data);
+            });
+        });
+    }
+
+    async function getDepartaments() {
+        return new Promise((resolve, reject) => {
+            let request = {
+                requestTimeout: 35000,
+                url: self.url,
+                method: 'department.get',
+                settings: {
+                    access_token: self.body['auth']['access_token'],
+                    BOT_ID: self.body.data.BOT[0].BOT_ID,
+					CHAT_ID: self.body.data.PARAMS.CHAT_ID
+                }
+            };
+
+            self.request(request, (err, data) => {
+                if (err) return reject(err);
+                resolve(data);
+            });
+        });
+    }
+
+    async function getDepartamentFields() {
+        return new Promise((resolve, reject) => {
+            let request = {
+                requestTimeout: 35000,
+                url: self.url,
+                method: 'department.fields',
+                settings: {
+                    access_token: self.body['auth']['access_token']
+                }
+            };
+
+            self.request(request, (err, data) => {
+                if (err) return reject(err);
+                resolve(data);
+            });
+        });
+    }
+
+    async function getContactFields() {
+        return new Promise((resolve, reject) => {
+            let request = {
+                requestTimeout: 35000,
+                url: self.url,
+                method: 'crm.contact.company.fields',
+                settings: {
+                    access_token: self.body['auth']['access_token']
+                }
+            };
+
+            self.request(request, (err, data) => {
+                if (err) return reject(err);
+                resolve(data);
+            });
+        });
+    }
+
+    async function getContactList() {
+        return new Promise((resolve, reject) => {
+            let request = {
+                requestTimeout: 35000,
+                url: self.url,
+                method: 'crm.contact.list',
+                settings: {
+                    access_token: self.body['auth']['access_token']
+                }
+            };
+
+            self.request(request, (err, data) => {
+                if (err) return reject(err);
+                resolve(data);
+            });
+        });
+    }
+
+    async function getContact() {
+        return new Promise((resolve, reject) => {
+            let request = {
+                requestTimeout: 35000,
+                url: self.url,
+                method: 'crm.contact.get',
+                settings: {
+                    access_token: self.body['auth']['access_token'],
+                    id: '54'
+                }
+            };
+
+            self.request(request, (err, data) => {
+                if (err) return reject(err);
+                resolve(data);
+            });
+        });
+    }
+
+    async function getProductCatalog() {
+        return new Promise((resolve, reject) => {
+            let request = {
+                requestTimeout: 35000,
+                url: self.url,
+                method: 'crm.catalog.list',
+                settings: {
+                    access_token: self.body['auth']['access_token']
+                }
+            };
+
+            self.request(request, (err, data) => {
+                if (err) return reject(err);
+                resolve(data);
+            });
+        });
+    }
+
+    async function getProductCatalogOnId() {
+        return new Promise((resolve, reject) => {
+            let request = {
+                requestTimeout: 35000,
+                url: self.url,
+                method: 'crm.catalog.get',
+                settings: {
+                    access_token: self.body['auth']['access_token'],
+                    id: '24'
+                }
+            };
+
+            self.request(request, (err, data) => {
+                if (err) return reject(err);
+                resolve(data);
+            });
+        });
+    }
+
+    async function getDescriptionProductCatalog() {
+        return new Promise((resolve, reject) => {
+            let request = {
+                requestTimeout: 35000,
+                url: self.url,
+                method: 'crm.catalog.fields',
+                settings: {
+                    access_token: self.body['auth']['access_token']
+                }
+            };
+
+            self.request(request, (err, data) => {
+                if (err) return reject(err);
+                resolve(data);
+            });
+        });
+    }
+
+    async function getDescriptionFieldsProduct() {
+        return new Promise((resolve, reject) => {
+            let request = {
+                requestTimeout: 35000,
+                url: self.url,
+                method: 'crm.product.fields',
+                settings: {
+                    access_token: self.body['auth']['access_token']
+                }
+            };
+
+            self.request(request, (err, data) => {
+                if (err) return reject(err);
+                resolve(data);
+            });
+        });
+    }
+
+    /*
     self.request({
         url: self.url,
         method: 'imbot.message.add',
@@ -48,6 +233,11 @@ exports.src = async function (self, cb) {
             if (err) return console.log(err);
         }
     );
+    */
+
+    function hasWhiteSpace(s) {
+	    return s.indexOf(' ') >= 0;
+    }
 
     switch(self.message) {
         case 'что горит':
@@ -60,12 +250,108 @@ exports.src = async function (self, cb) {
             cb(self);
             break;
 
+        case 'Список подразделений':
+            try {
+                self.answer = await getDepartaments();
+            } catch(err) {
+            }
+            cb(self);
+            break;
+
+        case 'Список полей подразделения':
+            try {
+                self.answer = await getDepartamentFields();
+            } catch(err) {
+            }
+            cb(self);
+            break;
+
+        case 'Список контактов':
+            try {
+                self.answer = await getContactList();
+            } catch(err) {
+            }
+            cb(self);
+            break;
+
+        case 'Получить контакт':
+            try {
+                self.answer = await getContact();
+            } catch(err) {
+            }
+            cb(self);
+            break;
+
+        case 'Список товарных каталогов':
+            try {
+                self.answer = await getProductCatalog();
+            } catch(err) {
+            }
+            cb(self);
+            break;
+
+        case 'Товарный каталог по id':
+            try {
+                self.answer = await getProductCatalogOnId();
+            } catch(err) {
+            }
+            cb(self);
+            break;
+
+        case 'Описание полей каталога товаров':
+            try {
+                self.answer = await getDescriptionProductCatalog();
+            } catch(err) {
+            }
+            cb(self);
+            break;
+
+        case 'Описание полей товара':
+            try {
+                self.answer = await getDescriptionFieldsProduct();
+            } catch(err) {
+            }
+            cb(self);
+            break;
+
+        case 'Здравствуйте':
+        case 'здравствуйте':
+        case 'Привет':
+        case 'привет':
+            self.answer = 'Здравствуйте!';
+            cb(self);
+            break;
+
+        case 'заткнись':
+
+            cb(self);
+            break;
+
+        case 'список':
+            try {
+                self.answer = await getBotList();
+            } catch(err) {
+                console.error(err);
+                self.answer = 'Ошибка при получении тасков';
+            }
+            cb(self);
+            break;
+
         default:
+            /*
             self.answer = await new Promise((resolve) => {
                 setTimeout( () => {
                     return resolve(`Стандартный ответ на сообщение: ${self.message}`);
                 }, 1000);
             });
+            */
+
+            if ( hasWhiteSpace(self.message) ) {
+                self.answer = 'Мы получили ваше обращение, ожидайте ответа.';
+            } else {
+                self.answer = 'Здравствуйте!';
+            }
+
             cb(self);
             break;
     }
