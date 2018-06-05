31605d2e73565067eba97fbf8f28a2f653382570
pro-ject@mail.ru <pro-ject@mail.ru>
2018-06-05 14:32:13 +0300

    web plug

--- modules/web/application/controller/statusSMPP.js
+++ modules/web/application/controller/statusSMPP.js
@@ -1,12 +1,21 @@
+/*
+ * statusUA module
+ */
+
+/**
+ * This method read records
+ *
+ * @param req
+ * @param res
+ */
 var bus;
-exports.init = function(_bus) {
+exports.init = function (_bus) {
     bus = _bus;
 };
 
-
-function getB24accounts() {
+function getSipClients() {
     return new Promise((resolve) => {
-        bus.request('b24accounts', {}, function (err, data) {
+        bus.request('sipClients', {}, function (err, data) {
             if (err) return resolve([]);
             if (data) {
                 return resolve(data);
@@ -16,58 +25,55 @@ function getB24accounts() {
     });
 }
 
-exports.getStoreData = async function() {
-    let data = await getB24accounts();
-
-    if (typeof data == 'string')
+exports.getStoreData = async function (data, clients) {
+    if (data){
         try {
             data = JSON.parse(data);
         } catch (e) {
             data = [];
         }
-
+    }
     var rec = [null, null, null, null, null, null, null, null, null, null,
-        null, null, null, null, null, null, null, null, null, null
-    ];
 
-    Object.keys(data).forEach(function(row, i) {
-        if ( data[row] && data[row].auth && ('disable' in data[row].auth) ) {
-            if (data[row].auth.disable == 1) {
-                rec[i] = 0;
-            } else {
-                if (data[row].auth.access_token) {
-                    rec[i] = 1;
-                } else {
-                    rec[i] = 2;
+                null, null, null, null, null, null, null, null, null, null];
+
+
+    var clients = clients || await getSipClients();
+
+    if (clients) {
+         clients.forEach(function (row, i) {
+            rec[i] = 0;
+            rec[10+i] = row.user;
+        });
+    }
+
+    if (data) {
+        data.forEach(function (row, i) {
+            for (var i = 10; i < rec.length; i++){
+                if (row.substr(0,row.indexOf('@')) == rec[i]){
+                    rec[i - 10] = 1;
+                    rec[i] = row;
                 }
             }
-        } else {
-            if (data[row] && data[row].auth && data[row].auth.access_token) {
-                rec[i] = 1;
-            } else {
-                rec[i] = 2;
-            }
-        }
-
-        /*
-        if ('disable' in data[row].auth) {
-            rec[i] = !data[row].auth.disable >>> 0;
-
-            // Не удалось зарегистрироваться
-            if (rec[i] && row && data && data[row] && data[row].auth && !data[row].auth.access_token) {
-                rec[i] = 2;
-            }
-        }
-        */
-
-        rec[(1 + '' + i)] = row;
-    });
+            // rec[i] = 1;
+            // rec[10+i] = row;
+        });
+    }
+    //bus.emit('message', {category: 'sip_proxy', type: 'trace', msg: rec });
     return [rec];
 };
 
-exports.read = async function(req, res) {
-    res.send({ 
-        success: true, 
-        data: await exports.getStoreData() 
+exports.read = function (req, res) {
+    bus.request('getStatusSipCliList', {}, function (errSipList, sipList) {
+        if (errSipList) return false;
+        if (sipList) {
+            // bus.request('sipServer', {}, function (errSipServer, sipServer) {
+            bus.request('sipClients', {}, async function (errSipServer, clients) {
+                if (errSipServer) return false;
+                res.send({success: true, data: await exports.getStoreData(JSON.stringify(sipList), clients)});
+            });
+        } else {
+            return false;
+        }
     });
 };
